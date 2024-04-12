8187921c0827489266d7b1de645432491bb5d8e4
sindhu-121 <sindhu.g@egradtutor.in>
2024-04-12 17:07:59 +0530

    hiiii

--- my-app/src/apiConfig.js
+++ my-app/src/apiConfig.js
@@ -1,4 +1,4 @@
 // apiConfig.js
-const BASE_URL = 'https://otsegt.azurewebsites.net'; // Your localhost URL
- 
+const BASE_URL = 'https://otsegt.azurewebsites.net'; 
+// const BASE_URL = 'http://localhost:8080'; 
 export default BASE_URL;
