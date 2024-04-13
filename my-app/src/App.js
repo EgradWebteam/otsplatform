48f8eeb1571d910701ed3ffe7c2140c2496ea3be
sindhu-121 <sindhu.g@egradtutor.in>
2024-04-13 11:37:08 +0530

    sindhu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,7 +19,7 @@ function App() {
         setLoading(false);
       });
   }, []);
-  
+
   return (
     <div className="App">
       <h1>Your React App</h1>
--- my-app/src/apiConfig.js
+++ my-app/src/apiConfig.js
@@ -1,4 +1,4 @@
 // apiConfig.js
-// const BASE_URL = 'https://otsegt.azurewebsites.net'; 
-const BASE_URL = 'http://localhost:8080'; 
+const BASE_URL = 'https://otsplatform.azurewebsites.net'; 
+// const BASE_URL = 'http://localhost:8080'; 
 export default BASE_URL;
