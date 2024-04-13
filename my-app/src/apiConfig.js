00cd80e8cffd22deb569aecb173eb505ed0f9e7a
sindhu-121 <sindhu.g@egradtutor.in>
2024-04-13 11:57:18 +0530

    hello

--- my-app/src/apiConfig.js
+++ my-app/src/apiConfig.js
@@ -1,4 +1,4 @@
 // apiConfig.js
-const BASE_URL = 'https://otsplatform.azurewebsites.net'; 
+const BASE_URL = 'https://otsplatform.azurewebsites.net/'; 
 // const BASE_URL = 'http://localhost:8080'; 
 export default BASE_URL;
